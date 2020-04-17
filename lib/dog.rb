140b0b66eac8314562c026721465a32692b831de
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-17 00:27:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+# Dog
+#   #initialize
+#     sets the name of the dog in an instance variable @name
+#     sets the breed of the dog in an instance variable @breed
+#     defaults the breed argument to "Mutt" in an instance variable @breed
+
+
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
